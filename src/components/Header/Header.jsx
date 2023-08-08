ce20e02d84dbb1752bab8011debf20539d5f98de
BrightIweobi <132474322+BrightifyInc@users.noreply.github.com>
2023-08-08 05:48:50 -0700

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -28,9 +28,9 @@ function Header() {
     <body>
         <div id="containerz">
 
-            <div id="loading">
+{/*             <div id="loading">
                 <span id="loading-img">Loading...</span>
-            </div>
+            </div> */}
 
         </div>
 
